4a0be2875d879d2ed06a9b9935db33b8cb3c1438
kovesdieszter <kovesdieszter@github.com>
2023-10-10 17:20:02 +0200

    feat: implement footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../style/Footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <p>© 2023 Eszter Kövesdi </p>
+    </div>
+  );
+};
