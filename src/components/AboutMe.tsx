e285dcb7519a57701e20388f4aca04dab6999ae0
kovesdieszter <kovesdieszter@github.com>
2023-10-13 11:45:36 +0200

    fix: about me info pictures

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,25 +1,23 @@
 import Hiking from "../assets/aboutme/hiking.jpg";
 import Cirrus from "../assets/aboutme/cirrus.jpg";
 import Dragon from "../assets/aboutme/dragon.jpg";
-import Shakshuka from "../assets/aboutme/shakshuka.jpg";
 
 import "../style/AboutMe.css";
 
 export const AboutMe = () => {
   return (
-    <div className="about-me-container">
-      <div className="image-card">
-        <img src={Hiking} alt="Hiking" />
+    <>
+      <div className="about-me-container">
+        <div className="image-card">
+          <img src={Hiking} alt="Hiking" />
+        </div>
+        <div className="image-card">
+          <img src={Cirrus} alt="Cirrus" />
+        </div>
+        <div className="image-card">
+          <img src={Dragon} alt="Dragon" />
+        </div>
       </div>
-      <div className="image-card">
-        <img src={Cirrus} alt="Cirrus" />
-      </div>
-      <div className="image-card">
-        <img src={Dragon} alt="Dragon" />
-      </div>
-      <div className="image-card">
-        <img src={Shakshuka} alt="Shakshuka" />
-      </div>
-    </div>
+    </>
   );
 };
